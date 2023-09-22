c6d959ffd46ea7f65e1dd926cc3c9b6d30f61259
ArmandoSilverioPatricio <ing.armando.silverio@outlook.com>
2023-09-22 15:52:42 -0600

    Agregando carpertas para ejercicios CRUD

---
+++ 1-BuscarRegistros/formularioBusqueda.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style.css">
+    <title>Formulario de busqueda</title>
+</head>
+<body>
+    <h1>Busqueda de registros</h1>
+    <br>
+    <form action="busqueda.php" method="get">
+        <label>Buscar:
+            <input type="text" name="buscar">
+            <input type="submit" value="Buscar !!!" name="enviado">
+        </label>
+    </form>
+
+</body>
+</html>
